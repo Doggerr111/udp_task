420da8a0aacc73f43fced0c48188c5758e5218c9
Doggerr111 <doggerr111@gmail.com>
2026-04-09 05:43:48 +0300

    feat: implement packet packing

--- Client/packetbuilder.h
+++ Client/packetbuilder.h
@@ -1,11 +1,14 @@
 #ifndef PACKETBUILDER_H
 #define PACKETBUILDER_H
+#include <QByteArray>
 
-
+class SensorData;
 class PacketBuilder
 {
 public:
-    PacketBuilder();
+    static QByteArray pack(const SensorData& data);
+private:
+    PacketBuilder() = delete;
 };
 
 #endif // PACKETBUILDER_H
