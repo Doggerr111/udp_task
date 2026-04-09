2a4482be6220c49ff1c98773718e305c78b1f380
Doggerr111 <doggerr111@gmail.com>
2026-04-09 08:09:54 +0300

    feat: add DTO structure

---
+++ common/datatypes.h
@@ -0,0 +1,15 @@
+#ifndef DATATYPES_H
+#define DATATYPES_H
+
+struct SensorData
+{
+    int x = 0;
+    int y = 0;
+    int v = 0;
+    int m = 0;
+    int s = 0;
+    double a = 0.0;
+    int p = 0;
+};
+
+#endif // DATATYPES_H
