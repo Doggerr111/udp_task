54980f02f2d67c4a7f5a8f7e8e32e13091e639df
Doggerr111 <doggerr111@gmail.com>
2026-04-09 21:20:26 +0300

    refactor: change data types to fixed-width and add operator== for testing

--- common/datatypes.h
+++ common/datatypes.h
@@ -3,13 +3,25 @@
 
 struct SensorData
 {
-    int x = 0;
-    int y = 0;
-    int v = 0;
-    int m = 0;
-    int s = 0;
-    double a = 0.0;
-    int p = 0;
+    uint8_t x;
+    int8_t y;
+    uint8_t v;
+    uint8_t m;
+    uint8_t s;
+    float a;      
+    uint8_t p;
+    
+    //для тестирования
+    bool operator==(const SensorData& other) const 
+    {
+        return x == other.x &&
+               y == other.y &&
+               v == other.v &&
+               m == other.m &&
+               s == other.s &&
+               std::abs(a - other.a) < 0.01f &&  
+               p == other.p;
+    }
 };
 
 #endif // DATATYPES_H
