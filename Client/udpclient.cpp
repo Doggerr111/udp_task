508418d8b110ede18e93920e031be5d60572e071
Doggerr111 <doggerr111@gmail.com>
2026-04-10 16:21:05 +0300

    feat: implement UdpClient for server communication

--- Client/udpclient.cpp
+++ Client/udpclient.cpp
@@ -1,6 +1,50 @@
-#include "udpsender.h"
+#include "udpclient.h"
 
-UDPSender::UDPSender()
+UDPClient::UDPClient(QObject *parent)
+    : QObject(parent)
 {
-
+    mSocket = new QUdpSocket(parent);
 }
+
+bool UDPClient::setServerParams(const QString &ip, quint16 port)
+{
+    QHostAddress address(ip);
+    if (address.isNull())
+        return false;
+    mServerAddress = address;
+    mServerPort = port;
+    return true;
+}
+
+void UDPClient::sendData(const QByteArray &bytes)
+{
+    qint64 sent = mSocket->writeDatagram(bytes, mServerAddress, mServerPort);
+    if (sent == -1)
+        emit error("Ошибка отправки данных");
+    else if (sent != bytes.size())
+        emit error("Отправлено не все байты");
+}
+
+
+void UDPClient::startListening()
+{
+    if (!mSocket->bind())
+    {
+        emit error("Не удалось привязать сокет для получения ответов");
+        return;
+    }
+    connect(mSocket, &QUdpSocket::readyRead, this, &UDPClient::onReadyRead);
+}
+
+void UDPClient::onReadyRead()
+{
+    while (mSocket->hasPendingDatagrams())
+    {
+        QByteArray buffer;
+        buffer.resize(mSocket->pendingDatagramSize());
+        mSocket->readDatagram(buffer.data(), buffer.size());
+        qDebug()<<buffer;
+        emit responseReceived(buffer);
+    }
+}
+
--- Client/udpclient.h
+++ Client/udpclient.h
@@ -1,11 +1,29 @@
-#ifndef UDPSENDER_H
-#define UDPSENDER_H
+#ifndef UDPClient_H
+#define UDPClient_H
+#include <QUdpSocket>
 
-
-class UDPSender
+class UDPClient: public QObject
 {
+    Q_OBJECT
 public:
-    UDPSender();
+    UDPClient(QObject* parent = nullptr);
+    bool setServerParams(const QString& ip, quint16 port);
+    void sendData(const QByteArray& bytes);
+    void startListening();
+
+private slots:
+    void onReadyRead();
+
+
+signals:
+    void error(const QString& message);
+    void responseReceived(const QByteArray& data);
+    void dataSent(const QByteArray& data);
+
+private:
+    quint16 mServerPort;
+    QHostAddress mServerAddress;
+    QUdpSocket* mSocket;
 };
 
-#endif // UDPSENDER_H
+#endif // UDPClient_H
