4381c4f25508ddaa3dadbd3e33228493be4ebc2c
Doggerr111 <doggerr111@gmail.com>
2026-04-10 16:13:28 +0300

    feat: add client data reading and response sending

--- Server/udpserver.cpp
+++ Server/udpserver.cpp
@@ -1,6 +1,51 @@
 #include "udpserver.h"
-
-UDPServer::UDPServer()
+UDPServer::UDPServer(QObject *parent)
+    : QObject(parent)
 {
+    mUdpSocket = new QUdpSocket(this);
+}
 
+bool UDPServer::startServer(quint16 port)
+{
+    if (!mUdpSocket->bind(QHostAddress::Any, port))
+        return false;   //bind не удался (порт занят, нет прав и т.д.)
+
+    connect(mUdpSocket, &QUdpSocket::readyRead, this, &UDPServer::onReadyRead);
+    return true;
+}
+
+void UDPServer::stopServer()
+{
+    if (mUdpSocket)
+    {
+        mUdpSocket->close();
+        disconnect(mUdpSocket, &QUdpSocket::readyRead, this, &UDPServer::onReadyRead);
+    }
+}
+
+bool UDPServer::sendResponse(const QByteArray &response, const QHostAddress &clientAddress, quint16 clientPort)
+{
+     auto size = mUdpSocket->writeDatagram(response, clientAddress, clientPort);
+     return size == response.size() ? true : false;
+}
+
+void UDPServer::onReadyRead()
+{
+    qDebug()<<"readyRead()";
+    while (mUdpSocket->hasPendingDatagrams())
+    {
+        QByteArray buffer;
+        buffer.resize(mUdpSocket->pendingDatagramSize());
+
+        QHostAddress clientAddress;
+        quint16 clientPort;
+
+        mUdpSocket->readDatagram(buffer.data(), buffer.size(), &clientAddress, &clientPort);
+
+        qDebug() << "Получено от" << clientAddress.toString() << clientPort;
+        qDebug() << "Данные:" << buffer.toHex();
+
+        //отправляем в контроллер
+        emit dataReceived(buffer, clientAddress, clientPort);
+    }
 }
--- Server/udpserver.h
+++ Server/udpserver.h
@@ -1,11 +1,22 @@
 #ifndef UDPSERVER_H
 #define UDPSERVER_H
+#include <QUdpSocket>
 
-
-class UDPServer
+class UDPServer: public QObject
 {
+    Q_OBJECT
 public:
-    UDPServer();
+    UDPServer(QObject* parent = nullptr);
+    bool startServer(quint16 port);
+    void stopServer();
+    bool sendResponse(const QByteArray& response, const QHostAddress& clientAddress, quint16 clientPort);
+private slots:
+    void onReadyRead();
+signals:
+    void dataReceived(const QByteArray& data, const QHostAddress& clientAddr,
+                      quint16 clientPort);
+private:
+    QUdpSocket* mUdpSocket;
 };
 
 #endif // UDPSERVER_H
